1855769cab1775151b8b872520eb68fa8981a04b
barc05 <benjaminarucutipa@gmail.com>
2026-05-06 20:55:10 -0400

    ReporteValidator
    Se sube archivo faltante "Reporte Validator"

--- src/main/java/com/example/demo/controller/ReporteController.java
+++ src/main/java/com/example/demo/controller/ReporteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,8 @@ import com.example.demo.service.ReporteService;
 
 @RestController
 @RequestMapping("/api/reportes") // Ruta personalizada para tu API DR
+@CrossOrigin(origins = "http://localhost:5173")
+
 public class ReporteController {
 
     private final ReporteService service;
