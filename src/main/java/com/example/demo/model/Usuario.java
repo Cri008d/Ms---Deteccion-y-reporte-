30f54336868a3225dbb60eb715eacbd6ff78ac16
Cri008d <cri.zunigam@duocuc.cl>
2026-05-29 02:56:51 -0400

    Add files via upload

---
+++ src/main/java/com/example/demo/model/Usuario.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUsuario;
+
+    @Column(length = 40, nullable = false)
+    private String nombre;
+
+    @Column(length = 40, nullable = false)
+    private String correo;
+
+    @Column(length = 30, nullable = false)
+    private String contraseña;
+
+}
