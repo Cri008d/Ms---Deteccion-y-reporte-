f27962e65d6860b00f731cacc4283e91f80c3f6e
Cri008d <cri.zunigam@duocuc.cl>
2026-06-12 15:08:34 -0400

    Update UsuarioController.java

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.model.Usuario;
 import com.example.demo.service.UsuarioService;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/usuarios")
 public class UsuarioController {
     
