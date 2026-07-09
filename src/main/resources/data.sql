ce917fe85dae4074d01693c9fdaa749f4b9c4ac7
barc05 <benjaminarucutipa@gmail.com>
2026-07-09 00:07:37 -0400

    nom sin nn

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO usuarios (nombre, correo, contrase_a, rol)
+INSERT INTO usuarios (nombre, correo, contrasena, rol)
 VALUES ('Bombero Oficial', 'bombero@valleseguro.cl', '$2a$10$wH6Z7xI.WNVp.eYPR72UoO5pB0N8Ivxw6A66mH77gR8YshM1u7Bpe', 'FUNCIONARIO')
 ON CONFLICT (correo) DO NOTHING;
