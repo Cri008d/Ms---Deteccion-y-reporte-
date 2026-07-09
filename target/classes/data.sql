34c3e70c71f963293e10b73ba72cd29d8b8caf15
barc05 <benjaminarucutipa@gmail.com>
2026-07-08 23:49:23 -0400

    usuario bombero

---
+++ target/classes/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO usuarios (id, nombre, correo, contrasena, rol) 
+VALUES (999, 'Bombero Oficial', 'bombero@valleseguro.cl', '$2a$10$wH6Z7xI.WNVp.eYPR72UoO5pB0N8Ivxw6A66mH77gR8YshM1u7Bpe', 'FUNCIONARIO');
